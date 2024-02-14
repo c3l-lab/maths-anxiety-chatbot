45fe729c50029652637d23585f31b42c7a1a3589
Leon Romo <romoleon9@gmail.com>
2024-02-14 20:35:43 +1030

    Add redirect requests to root URL to login page

--- anxiety_chatbot_project/urls.py
+++ anxiety_chatbot_project/urls.py
@@ -1,10 +1,15 @@
-from django.contrib import admin
-from django.urls import path
-from chatbot.views import chat
+from django.urls import path, reverse_lazy
+from django.views.generic.base import RedirectView
 from authentication.views import login_view
+from chatbot.views import chat
 
 urlpatterns = [
-    path('chat/', chat, name='chat'),  # URL pattern for the chat interface
-     path('login/', login_view, name='login'), # URL pattern for the login page
-    path('', chat, name='default_chat'),  # Default URL pattern to redirect requests to the chat interface
+    # URL pattern for the chat interface
+    path('chat/', chat, name='chat'),
+    
+    # URL pattern for the login page
+    path('login/', login_view, name='login'),
+    
+    # Redirect requests to the root URL to the login page
+    path('', RedirectView.as_view(url=reverse_lazy('login')), name='root'),
 ]
