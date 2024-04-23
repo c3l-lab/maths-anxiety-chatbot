59466921d60fd38de2cf117a55d0a1baeeb87f2a
Tom Barone <mail@tombarone.net>
2024-04-23 16:19:34 +0930

    Don't keep cycling through responses once exhausted

--- app/models/chatbot.rb
+++ app/models/chatbot.rb
@@ -2,7 +2,9 @@
 
 require 'csv'
 class Chatbot < ApplicationRecord
-  CHATBOT_TYPES = ['Group A - Helpful Chatbot', 'Group B - Neutral Chatbot'].freeze
+  HELPFUL_GROUP = 'Group A'
+  NEUTRAL_GROUP = 'Group B'
+  CHATBOT_TYPES = [HELPFUL_GROUP, NEUTRAL_GROUP].freeze
 
   # Used to export all the chatbots to a CSV file
   def self.to_csv
@@ -16,11 +18,13 @@ class Chatbot < ApplicationRecord
         case header when 'id' then 'database_id' when 'conversation' then 'conversation (JSON)' else header end
       end
       header_row << 'Number of strategies / comments requested'
+      header_row << 'Reached response limit?'
       csv << header_row
 
       chatbots.each do |chatbot|
         value_row = chatbot.attributes.values_at(*output_columns)
         value_row << chatbot.number_of_strategies_requested
+        value_row << (chatbot.out_of_responses? ? 'Yes' : 'No')
         csv << value_row
       end
     end
@@ -29,8 +33,13 @@ class Chatbot < ApplicationRecord
   def number_of_strategies_requested
     return 0 if conversation.blank?
 
+    # Count the number of times a response was given
     conversation.count do |message|
-      message['from'] == 'user' && (message['message'] == 'Yes' || message['message'] == USER_HEAR_MORE)
+      if chatbot_type == HELPFUL_GROUP
+        HELPFUL_RESPONSES.include?(message['message'])
+      elsif chatbot_type == NEUTRAL_GROUP
+        NEUTRAL_RESPONSES.include?(message['message'])
+      end
     end
   end
 
@@ -40,14 +49,24 @@ class Chatbot < ApplicationRecord
     conversation.last['message'] == NO_RESPONSE
   end
 
+  def out_of_responses?
+    return false if conversation.blank?
+
+    if chatbot_type == HELPFUL_GROUP
+      conversation.last['message'] == HELPFUL_NO_MORE_RESPONSES
+    elsif chatbot_type == NEUTRAL_GROUP
+      conversation.last['message'] == NEUTRAL_NO_MORE_RESPONSES
+    end
+  end
+
   def start
     update!(conversation_started_at: DateTime.now)
     conversation = []
 
-    if chatbot_type == 'Group A - Helpful Chatbot'
+    if chatbot_type == HELPFUL_GROUP
       conversation << { message: HELPFUL_WELCOME_1, from: 'chatbot' }
       conversation << { message: HELPFUL_WELCOME_2, from: 'chatbot' }
-    elsif chatbot_type == 'Group B - Neutral Chatbot'
+    elsif chatbot_type == NEUTRAL_GROUP
       conversation << { message: NEUTRAL_WELCOME_1, from: 'chatbot' }
       conversation << { message: NEUTRAL_WELCOME_2, from: 'chatbot' }
     end
@@ -56,15 +75,7 @@ class Chatbot < ApplicationRecord
 
   def yes
     conversation << { message: 'Yes', from: 'user' }
-    conversation << { message: next_message, from: 'chatbot' }
-    prompt = if chatbot_type == 'Group A - Helpful Chatbot'
-               HELPFUL_PROMPT
-             elsif chatbot_type == 'Group B - Neutral Chatbot'
-               NEUTRAL_PROMPT
-             end
-    conversation << { message: prompt, from: 'chatbot' }
-    save!
-    sleep 0.1 # Simulate a delay
+    next_message
   end
 
   def no
@@ -76,15 +87,7 @@ class Chatbot < ApplicationRecord
 
   def hear_more
     conversation << { message: USER_HEAR_MORE, from: 'user' }
-    conversation << { message: next_message, from: 'chatbot' }
-    prompt = if chatbot_type == 'Group A - Helpful Chatbot'
-               HELPFUL_PROMPT
-             elsif chatbot_type == 'Group B - Neutral Chatbot'
-               NEUTRAL_PROMPT
-             end
-    conversation << { message: prompt, from: 'chatbot' }
-    save!
-    sleep 0.1 # Simulate a delay
+    next_message
   end
 
   # Have we started a conversation with this chatbot?
@@ -93,33 +96,43 @@ class Chatbot < ApplicationRecord
   end
 
   def next_message
-    existing_messages = conversation.map { |message| message['message'] }
-    available_responses = if chatbot_type == 'Group A - Helpful Chatbot'
-                            HELPFUL_RESPONSES
-                          elsif chatbot_type == 'Group B - Neutral Chatbot'
-                            NEUTRAL_RESPONSES
-                          end
-
-    # Return a response that has the lowest count
-    lowest_count = available_responses.map { |response| existing_messages.count(response) }.min
-    available_responses.find { |response| existing_messages.count(response) == lowest_count }
+    # Add the next message based on the user response and the chatbot type
+    # Pretty hacky but it's a once off thing so it doesn't matter too much
+    if chatbot_type == HELPFUL_GROUP
+      if number_of_strategies_requested >= HELPFUL_RESPONSES.length
+        conversation << { message: HELPFUL_NO_MORE_RESPONSES, from: 'chatbot' }
+      else
+        conversation << { message: HELPFUL_RESPONSES[number_of_strategies_requested], from: 'chatbot' }
+        conversation << { message: HELPFUL_PROMPT, from: 'chatbot' }
+      end
+    elsif chatbot_type == NEUTRAL_GROUP
+      if number_of_strategies_requested >= NEUTRAL_RESPONSES.length
+        conversation << { message: NEUTRAL_NO_MORE_RESPONSES, from: 'chatbot' }
+      else
+        conversation << { message: NEUTRAL_RESPONSES[number_of_strategies_requested], from: 'chatbot' }
+        conversation << { message: NEUTRAL_PROMPT, from: 'chatbot' }
+      end
+    end
+    save!
+    sleep 0.1 # Simulate a delay
   end
 
-  HELPFUL_WELCOME_1 = "Hi there, I'm Chatty the Chatbot. I'll be here to keep you company throughout your test and provide some extra support"
-  HELPFUL_WELCOME_2 = 'Would you like to hear some strategies to help you with your test?'
+  HELPFUL_WELCOME_1 = "Hi there, I'm Chatty the Chatbot. Taking a math test can be a stressful and anxiety-provoking task for many people. I'll be here to keep you company throughout your test and provide some extra support."
+  HELPFUL_WELCOME_2 = 'Would you like to hear a strategy for anxiety reduction?'
   HELPFUL_PROMPT = 'Would you like to hear another strategy?'
   HELPFUL_RESPONSES = [
-    'Taking a math test can be a stressful and anxiety-provoking task for many people. Research suggests that deep breathing can help reduce these feelings of stress or anxiety. Try slowly inhaling through your nose and exhaling through your mouth 10 times before returning to the exercise.',
-    'Taking a math test can be a stressful and anxiety-provoking task for many people. Research has shown us that by reframing our negative thoughts as positive thoughts, we can actually perform better. Try approaching the next question with the mindset that the question is challenging, rather than threatening, and that you have the ability to tackle it.',
-    'Taking a math test can be a stressful and anxiety-provoking task for many people, but research has demonstrated the impressive power of mental imagery. Try visualizing yourself solving the next problem. It may just give you the confidence boost you need to succeed.',
-    'Taking a math test can be a stressful and anxiety-provoking task for many people, but research has proven that a confidence boost can go a long way. Try starting with the questions you feel comfortable with, before moving on to tackling the ones you are feeling unsure about.',
-    'Taking a math test can be a stressful and anxiety-provoking task for many people. Research has proven that taking time for your body to relax or recharge can reduce anxiety and improve performance. Try taking a short break, standing up, or performing some stretches before returning to the exercise.',
-    "Taking a math test can be a stressful and anxiety-provoking task for many people, and research has indicated that when we're anxious we tend to focus on our negative experiences. However, focusing on positive experiences can remind us of our strengths. Try thinking of some examples of times you've had a successful or enjoyable interaction with math before returning to the exercise."
+    'Research suggests that deep breathing can help reduce these feelings of stress or anxiety. Try slowly inhaling through your nose and exhaling through your mouth 10 times before returning to the exercise.',
+    'Research has shown us that by reframing our negative thoughts as positive thoughts, we can actually perform better. Try approaching the next question with the mindset that the question is challenging, rather than threatening, and that you have the ability to tackle it.',
+    'Research has demonstrated the impressive power of mental imagery. Try visualizing yourself solving the next problem. It may just give you the confidence boost you need to succeed.',
+    'Research has proven that a confidence boost can go a long way. Try starting with the questions you feel comfortable with, before moving on to tackling the ones you are feeling unsure about.',
+    'Research has proven that taking time for your body to relax or recharge can reduce anxiety and improve performance. Try taking a short break, standing up, or performing some stretches before returning to the exercise.',
+    "Research has indicated that when we're anxious we tend to focus on our negative experiences. However, focusing on positive experiences can remind us of our strengths. Try thinking of some examples of times you've had a successful or enjoyable interaction with math before returning to the exercise."
   ].freeze
+  HELPFUL_NO_MORE_RESPONSES = "I'm sorry, I don't have any more strategies to share with you."
 
   NO_RESPONSE = "Sounds like you've got this covered! Just let me know if you'd like to hear more."
 
-  NEUTRAL_WELCOME_1 = "Hi there, I'm Chatty the Chatbot. I'll be here to keep you company throughout your test"
+  NEUTRAL_WELCOME_1 = "Hi there, I'm Chatty the Chatbot. Taking a math test can be a stressful and anxiety-provoking task for many people. I'll be here to keep you company throughout your test."
   NEUTRAL_WELCOME_2 = 'Would you like to hear a comment about research studies?'
   NEUTRAL_PROMPT = 'Would you like to hear another comment?'
   NEUTRAL_RESPONSES = [
@@ -130,6 +143,7 @@ class Chatbot < ApplicationRecord
     'There are many moving parts involved in creating a research study. These moving parts help ensure that the study runs smoothly and safely for each participant.',
     'Thanks for taking part in our study today!'
   ].freeze
+  NEUTRAL_NO_MORE_RESPONSES = "I'm sorry, I don't have any more comments to share with you."
 
   USER_HEAR_MORE = "I'd like to hear more"
 end
